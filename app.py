8941f0bd8f3680ec02770fc2619973e190aa03d8
Mikhail Bogomolov <mibogomolov@ozon.ru>
2023-06-25 21:48:59 +0300

    Change default handler for health check

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return jsonify({"message": "Hello, world!"})
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "OK"})
 
 if __name__ == '__main__':
     app.run(port=8080)
