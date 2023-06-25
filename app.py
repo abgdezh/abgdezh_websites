206bcf5aca16c67de07555efb855afd309034709
Mikhail Bogomolov <mibogomolov@ozon.ru>
2023-06-25 23:28:16 +0300

    Add / page with hello world

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def main_page():
+    return jsonify({"body": "Hello, world!"})
+
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "OK"})
